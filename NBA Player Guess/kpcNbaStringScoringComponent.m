054fb3af400b712904add310d23b52b1b0363c0b
Kevin Connor <kevinpaulconnor@gmail.com>
2013-12-08 11:49:24 -0800

    add kpcNBAStringScoringComponent

---
+++ NBA Player Guess/kpcNbaStringScoringComponent.m
@@ -0,0 +1,18 @@
+//
+//  kpcNbaStringScoringComponent.m
+//  NBA Player Guess
+//
+//  Created by Kevin Connor on 12/8/13.
+//  Copyright (c) 2013 Kevin Connor. All rights reserved.
+//
+
+#import "kpcNbaStringScoringComponent.h"
+
+@implementation kpcNbaStringScoringComponent
+
+- (kpcNbaStringScoringComponent*) init:(NSString*)data {
+    self = [super init];
+    return self;
+}
+
+@end
